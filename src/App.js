9abda50275b5bea9c88907b0eae2e4a4c01da359
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-05-15 21:13:15 +0530

    feat:added common navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, NavLink} from "react-router-dom"
 import Mockman from "mockman-js"
+
+
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductLists";
 
 function App() {
   return (
     <div className="App">
+    <nav>
+      <NavLink to = "/">Home</NavLink>|| 
+      <NavLink to = "/products">Our Products</NavLink>|| 
+      <NavLink to = "/wishlist">Wishlist</NavLink>|| 
+      <NavLink to= "/cart">Cart</NavLink>|| 
+      <NavLink to ="/user_account">My Account</NavLink>
+    </nav>
 
     <Routes>
       <Route path="/" element={<Home/>}/>
