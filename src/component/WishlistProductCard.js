752cb7ec2e2b1ddb3f9e47d04c80abf1fdc91114
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-05-25 01:38:27 +0530

    styles:wishlist page

--- src/component/WishlistProductCard.js
+++ src/component/WishlistProductCard.js
@@ -1,10 +1,16 @@
 import { useProductContext } from "../context/product-context";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import "../styles/wishlist.css";
 
 export default function WishlistProductCard(item) {
-  const { removeFromWishlist, state, findInCart, addToCart } =
-    useProductContext();
+  const {
+    removeFromWishlist,
+    state,
+    findInCart,
+    addToCart,
+    findInWishlist,
+    addToWishlist,
+  } = useProductContext();
 
   const { _id, title, price, img } = item;
 
@@ -20,20 +26,32 @@ export default function WishlistProductCard(item) {
     }
   };
 
+  const indexWishlist = findInWishlist(_id);
+  const handleClickForWishlist = () => {
+    if (indexWishlist === -1) {
+      addToWishlist(item);
+    } else {
+      removeFromWishlist(item._id);
+    }
+  };
+
   return (
     <div className="wishlist-card">
-      <img src={img} style={{ width: "100%" }} />
+      <span
+        role="button"
+        className="icon-background"
+        onClick={() => removeFromWishlist(_id)}
+      >
+        <i class="fa fa-close" style={{ fontSize: "24px", color: "black" }}></i>
+      </span>
+      <NavLink to={`/details/${item._id}`}>
+        <img src={img} style={{ width: "100%" }} />
+      </NavLink>
+
       <p className="wishlist-title">{title}</p>
       <p className="price">Rs. {price}</p>
 
       <div className="wishlist-btn-container">
-        <button
-          className="wishlist-btn"
-          style={{ backgroundColor: "black", color: "white" }}
-          onClick={() => removeFromWishlist(_id)}
-        >
-          Remove
-        </button>
         <button
           className="wishlist-btn"
           onClick={handleClickForCart}
